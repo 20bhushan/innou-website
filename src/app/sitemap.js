88a778ead7cb3dd6c1b917283d54d9ae1f54e7bd
Bhushan RK <124965063+20bhushan@users.noreply.github.com>
2026-03-18 01:22:54 +0000

    add sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://innou.in",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://innou.in/schedule",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://innou.in/competitions",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://innou.in/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
